999b8703e1c7cf32892cb5919035911e746d1a79
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2019-01-02 01:49:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-console.log("Welcome Again !");
  
  client.on("guildMemberRemove", member => {
   member.createDM().then(function (channel) {
   return channel.send(` 
 **
-سلام عليكم يا قلبي 
-لو سمحت طالبق طلب تكقا ادخل السيرفر
-                               [ رابط السيرفر ] 
+لو سمحت حبيب قلبي تقدر تدخل سيرفري
+                               [ https://discord.gg/PDmqhcP ] 
 الدعووة خاصة لك ي قلبي ... [ ${member}  ]
 **`)
 }).catch(console.error)
@@ -20,11 +18,12 @@ client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
   return channel.send(` 
 **
-سلام عليكم يا قلبي 
-لو سمحت طالبق طلب تكقا ادخل السيرفر
-                               [ رابط السيرفر ] 
+لو سمحت حبيب قلبي تقدر تدخل سيرفري
+                               [ https://discord.gg/PDmqhcP ] 
 الدعووة خاصة لك ي قلبي ... [ ${member}  ]
 **`) 
 }).catch(console.error)
 })
-client.login('توكين');
+
+
+client.login(process.env.BOT_TOKEN);
